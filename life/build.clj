8a0e521073a19efecfb2649ab99389c754c5e518
Mitsuru Gima <gima0326@yahoo.co.jp>
2020-03-10 20:05:25 +0900

    Add files via upload

--- life/build.clj
+++ life/build.clj
@@ -1,4 +1,16 @@
+;;====================================
+;;
+;; life/
+;; ├─ out/
+;; ├─ src/
+;; │　　└─ life/
+;; │　　　　└─ core.cljs
+;; ├─ index.html
+;; ├─ build.clj
+;; └─ cljs.jar  [ https://github.com/clojure/clojurescript/releases/download/r1.10.520/cljs.jar ]
+;;
+;;====================================
+
 (require 'cljs.build.api)
 (cljs.build.api/build "src" {:main 'life.core :output-to "out/main.js"})
 ;;(cljs.build.api/watch "src" {:main 'life.core :output-to "out/main.js"})
-
