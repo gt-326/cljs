eadbb49469400a57dd145424de04cc70171afbf9
Mitsuru Gima <gima0326@yahoo.co.jp>
2020-06-15 09:43:49 +0900

    Add files via upload

--- life/src/life/core.cljs
+++ life/src/life/core.cljs
@@ -53,79 +53,83 @@
 ;; Any live cell with more than three live neighbours dies, as if by overcrowding.
 ;; Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
-(defn cntNeighbors [g row col m_row m_col]
+(defn cntNeighbors [g row col]
 　(let [neighbors [[-1 -1] [0 -1] [1 -1] [1 0] [1 1] [0 1] [-1 1] [-1 0]]]
 　　(count (remove zero?
 　　　(for [[i j] neighbors :let [h (+ row i) w (+ col j)]]
-　　　　(if (and (< 0 h m_row) (< 0 w m_col))
+　　　　(if (and (< 0 h ROWS) (< 0 w COLS))
 　　　　　(get-in @g [h w])
 　　　　　STATE-DEAD))))))
 
-(defn updateView [g m n]
-　;; Does anyone know how to display result without using .log ?
-　(.log js/console (str "updateView:" (util/nested-vector m n
-　　(fn [i j &　_]
+(defn updateView [g]
+　;; Does anyone know how to display new status without using .log ?
+  (util/console-log "updateView"
+　　(fn [i j]
 　　　(let [stat (get-in @g [i j])]
 　　　　(do
 　　　　　(setCellAttributeClass i j stat)
-　　　　　stat)))))))
+　　　　　stat)))))
 
-(defn computeNextGen [g m n]
+(defn computeNextGen [g]
 　(do
 　　;; copy state-next to grid
-　　(reset! g (util/nested-vector m n
-　　　(fn [i j m n]
-　　　　(case (cntNeighbors g i j m n)
+　　(reset! g (util/nested-vector
+　　　(fn [i j]
+　　　　(case (cntNeighbors g i j)
 　　　　　2 (if (= (get-in @g [i j]) STATE-DEAD) STATE-DEAD STATE-ALIVE)
 　　　　　3 STATE-BORN
 　　　　　STATE-DEAD))))
 
 　　;; copy all 1 values to "live" in the table
-　　(updateView g m n)))
+　　(updateView g)))
 
-(defn play [playing timer g m n]
+(defn play [playing timer g]
 　(if @playing
 　　(do
-　　　(computeNextGen g m n)
-　　　(reset! timer (js/setTimeout #(play playing timer g m n) RE-PRODUCTION-TIME)))))
+　　　(computeNextGen g)
+　　　(reset! timer (js/setTimeout #(play playing timer g) RE-PRODUCTION-TIME)))))
 
 ;; start game
-(defn startButtonHandler [playing timer g obj m n]
+(defn startButtonHandler [playing timer g obj]
 　(let [flg @playing]
 　　(do
-　　　(.log js/console (if flg "Pause the game" "Continue the game"))
+　　　(util/console-log (if flg "Pause the game" "Continue the game"))
 　　　(reset! playing (not flg))
 　　　(set! (.-innerHTML obj) (if flg "Restart" "Stop"))
-　　　(if flg (.clearTimeout js/window @timer) (play playing timer g m n)))))
+　　　(if flg (.clearTimeout js/window @timer) (play playing timer g)))))
 
 ;; clear the grid
-(defn clearButtonHandler [playing timer g init m n]
+(defn clearButtonHandler [playing timer g init]
 　(do
-　　(.log js/console "Clear the game: stop playing, clear the grid")
+　　(util/console-log "Clear the game: stop playing, clear the grid")
 　　(reset! playing false)
 　　(set! (.-innerHTML (getHtmlElementById "start")) "Start")
 　　(.clearTimeout js/window @timer)
+
 　　;; init grid
 　　(reset! g init)
-　　;; Does anyone know how to display result without using .log ?
-　　(.log js/console (str "grid_dead:" (util/nested-vector m n
-　　　(fn [i j & _] (do (setCellAttributeClass i j STATE-DEAD) STATE-DEAD)))))))
+
+　　;; Does anyone know how to display new status without using .log ?
+　　(util/console-log "grid_dead"
+　　　(fn [i j] (do (setCellAttributeClass i j STATE-DEAD) STATE-DEAD)))))
 
 ;; calc random value
-(defn randomButtonHandler [playing timer g init m n]
+(defn randomButtonHandler [playing timer g init]
 　(if (not @playing)
 　　(do
 　　　;; clear Grid
-　　　(clearButtonHandler playing timer g init m n)
+　　　(clearButtonHandler playing timer g init)
+
 　　　;; init grid randomValue
-　　　(reset! g (util/nested-vector m n #(.round js/Math (js/Math.random))))
-　　　;; Does anyone know how to display result without using .log ?
-　　　(.log js/console (str "state:" (util/nested-vector m n
-　　　　(fn [i j & _]
+　　　(reset! g (util/nested-vector #(.round js/Math (js/Math.random))))
+
+　　　;; Does anyone know how to display new status without using .log ?
+　　　(util/console-log "state"
+　　　　(fn [i j]
 　　　　　(let [stat (get-in @g [i j])]
 　　　　　　(do
 　　　　　　　(if (= stat STATE-BORN) (setCellAttributeClass i j STATE-ALIVE))
-　　　　　　　stat)))))))))
+　　　　　　　stat)))))))
 
 ;;=== Grid cell click event ===========
 
@@ -137,36 +141,34 @@
 
 ;;=== PageLoad etc ====================
 
-(defn createGc [g m n]
+(defn createGc [g]
 　(let [gc (getHtmlElementById "gridContainer")]
 　　(if gc
-　　　;; Does anyone know how to display result except .log ?
-　　　(.log js/console (str "tbl:"
-　　　　(let [tbl (createHtmlElementAsChild gc "table")]
-　　　　　(for [i (range 0 m) :let [tr (createHtmlElementAsChild tbl "tr")]]
-　　　　　　(for [j (range 0 n) :let [td (createHtmlElementAsChild tr "td")]]
-　　　　　　　(do
-　　　　　　　　(setAttribute td "id" (str i SEP-CHAR j))
-　　　　　　　　(setObjectAttributeClass td STATE-DEAD)
-　　　　　　　　(set! (.-onclick td) #(cellClickHandler [i j] g td)))))))))))
+　　　;; Does anyone know how to display new status without using .log ?
+　　　(util/console-log "html_tbl" gc 
+　　　　(fn [i j obj]
+　　　　　(do
+　　　　　　(setAttribute obj "id" (str i SEP-CHAR j))
+　　　　　　(setObjectAttributeClass obj STATE-DEAD)
+　　　　　　(set! (.-onclick obj) #(cellClickHandler [i j] g obj))))))))
 
-(defn setupControlButtons [g init m n]
+(defn setupControlButtons [g init]
 　(let [ flg (atom false)
          timer (atom nil)
 　　　　　btn1 (getHtmlElementById "start")
 　　　　　btn2 (getHtmlElementById "clear")
 　　　　　btn3 (getHtmlElementById "random") ]
 　　(do
-　　　(set! (.-onclick btn1) #(startButtonHandler flg timer g btn1 m n))
-　　　(set! (.-onclick btn2) #(clearButtonHandler flg timer g init m n))
-　　　(set! (.-onclick btn3) #(randomButtonHandler flg timer g init m n)))))
+　　　(set! (.-onclick btn1) #(startButtonHandler flg timer g btn1))
+　　　(set! (.-onclick btn2) #(clearButtonHandler flg timer g init))
+　　　(set! (.-onclick btn3) #(randomButtonHandler flg timer g init)))))
 
 ;; Initialize
-(defn initialize [r c]
-　(let [init (util/nested-vector r c (fn [] STATE-DEAD)) g (atom init)]
+(defn initialize []
+　(let [init (util/nested-vector (fn [] STATE-DEAD)) g (atom init)]
 　　(do
-　　　(createGc g r c)
-　　　(setupControlButtons g init r c))))
+　　　(createGc g)
+　　　(setupControlButtons g init))))
 
 ;; Start everything
-(set! (.-onload js/window) #(initialize ROWS COLS))
+(set! (.-onload js/window) #(initialize))
--- life/src/life/util.clj
+++ life/src/life/util.clj
@@ -1,5 +1,16 @@
 (ns life.util)
 
-(defmacro nested-vector [r c fnc]
-　`(vec (for [~'i (range 0 ~r)]
-　　(vec (for [~'j (range 0 ~c)] (~fnc ~'i ~'j ~r ~c))))))
+(defmacro nested-vector [fnc]
+　`(vec (for [~'i (range 0 ~'ROWS)]
+　　(vec (for [~'j (range 0 ~'COLS)] (~fnc ~'i ~'j))))))
+
+(defmacro nested-vector2 [gc fnc]
+　`(let [tbl# (~'createHtmlElementAsChild ~gc "table")]
+　　(vec (for [~'i (range 0 ~'ROWS) :let [~'tr (~'createHtmlElementAsChild tbl# "tr")]]
+　　　(vec (for [~'j (range 0 ~'COLS) :let [~'td (~'createHtmlElementAsChild ~'tr "td")]]
+　　　　(~fnc ~'i ~'j ~'td)))))))
+
+(defmacro console-log
+  ([title obj fnc] `(console-log (str ~title ":" (nested-vector2 ~obj ~fnc))))
+  ([title fnc] `(console-log (str ~title ":" (nested-vector ~fnc))))
+  ([msg] `(.log js/console ~msg)))
